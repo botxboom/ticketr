c180f3c8977a0fd26e98248019d126d173ae1c1a
Gulshan Kumar <kumarjhagulshan@gmail.com>
2024-12-15 23:25:38 +0530

    add edit event

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "woozy-clam-99.convex.cloud",
+        protocol: "https",
+      },
+    ],
+  },
   /* config options here */
 };
 
